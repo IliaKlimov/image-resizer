cf38a4049207773fbf14b91896947fe942e1d671
Ilia Klimov <infuzer@gmail.com>
2020-08-28 14:38:13 +0300

    added docker files, instruction for setting up

--- img_resize/models.py
+++ img_resize/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from .storage import OverwriteStorage
+import os
 
 class Image(models.Model):
     img = models.ImageField(upload_to="img/original")
     img_resized = models.ImageField(upload_to="img/resized", storage=OverwriteStorage(), blank=True)
 
     def __str__(self):
-        return self.img.name.split("/")[-1]
+        return os.path.basename(self.img.name)
 
     def save(self, *args, **kwargs):
         if not self.img_resized:
